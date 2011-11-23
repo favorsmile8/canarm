29e5b923df23ad5965ed29de7f0d95bbd56da443
syl20.marchand <syl20.marchand@897a6eea-830f-63a3-b595-cfc6d0276e49>
2011-11-23 22:49:04 +0000

    add read and write functions and better Makefile

--- trunk/src/lib/canTransfert.h
+++ trunk/src/lib/canTransfert.h
@@ -2,5 +2,11 @@
  * Header file containing the CAN bus relative functions
  */
 
+#ifndef _CAN_TRANSFERT_H__
+#define _CAN_TRANSFERT_H__
+
 int can_init();
 int can_release(int fd);
+int can_write(char* buf, int len, int fd);
+
+#endif /*_CAN_TRANSFERT_H__*/
