7e4669bf019b7e235daf301c90888685476ba68f
syl20.marchand <syl20.marchand@897a6eea-830f-63a3-b595-cfc6d0276e49>
2011-12-10 23:22:03 +0000

    init of ecan (not tested)

--- src/CAN_dsPIC33F.X/main.c
+++ src/CAN_dsPIC33F.X/main.c
@@ -34,6 +34,24 @@ int main()
 
     CAN1Initialize(config1, config2);
 
+    /*Configure FIFO*/
+    config1 = CAN_DMA_BUF_SIZE_32 & CAN_FIFO_AREA_RB9;
+
+    CAN1FIFOCon(config1);
+
+    /*Set FIFO buffer pointers*/
+    CAN1SetBUFPNT1(0x0123);
+    CAN1SetBUFPNT2(0x4567);
+    CAN1SetBUFPNT3(0x89ab);
+    CAN1SetBUFPNT4(0xcdef);
+
+    /*Configure filters, use of filter 1
+     *For other devices type, if the information to get or set are not the same
+     *other filter numbers could be used
+     */
+    config1 = CAN_FILTER_SID(1) & CAN_RX_EID_DIS;
+    CAN1SetFilter(1, config1, CAN_FILTER_EID(0));
+
     /*Set operation mode : loopback to test -> put it in normal when it works*/
     config1 = CAN_IDLE_STOP &
             CAN_REQ_OPERMODE_LOOPBK &
@@ -42,12 +60,6 @@ int main()
     config2 = CAN_CMP_DATABYTE1_BIT7_EID0;
 
     CAN1SetOperationMode(config1, config2);
-
-    /*Configure FIFO*/
-
-    /*Configure filters*/
-
-    /*Set buffer pointers*/
 
     /*loop*/
     while(1){
