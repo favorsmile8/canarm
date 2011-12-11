bd2caec3168c27703126feb999650bed07fc2844
syl20.marchand <syl20.marchand@897a6eea-830f-63a3-b595-cfc6d0276e49>
2011-12-11 17:18:16 +0000

    first try with loopback mode (not tested)

--- trunk/src/CAN_dsPIC33F.X/main.c
+++ trunk/src/CAN_dsPIC33F.X/main.c
@@ -12,6 +12,7 @@ _FOSC(POSCMD_HS);
 #endif
 
 #include<ecan.h>
+#include<string.h>
 
 void LEDon(int on);
 
@@ -19,6 +20,12 @@ int main()
 {
     unsigned int config1 = 0;
     unsigned int config2 = 0;
+    unsigned char data[8];
+    unsigned char dataTX[8];
+    int i = 0;
+
+    memset(data, 0, 8);
+    memset(dataTX, 'a', 8);
 
     /*Initialise ECAN module
      *With ecan.h functions I hope DMA is initialised
@@ -39,18 +46,30 @@ int main()
 
     CAN1FIFOCon(config1);
 
-    /*Set FIFO buffer pointers*/
-    CAN1SetBUFPNT1(0x0123);
-    CAN1SetBUFPNT2(0x4567);
-    CAN1SetBUFPNT3(0x89ab);
-    CAN1SetBUFPNT4(0xcdef);
+    /*Set FIFO buffer pointers
+     *Each message types needs 1 TX buffer and 1 RX buffer
+     */
+    CAN1SetBUFPNT1(0x1122);
+    CAN1SetBUFPNT2(0x3344);
+    CAN1SetBUFPNT3(0x5566);
+    CAN1SetBUFPNT4(0x7788);
+
+    /*Set the direction of each buffer*/
+    for(i=0 ; i<16 ; i++){
+        /*buffer 0 : RX
+         *buffer 1 : TX
+         *...
+         */
+        CAN1SetTXRXMode(i, i%2);
+    }
 
     /*Configure filters, use of filter 1
      *For other devices type, if the information to get or set are not the same
      *other filter numbers could be used
      */
     config1 = CAN_FILTER_SID(1) & CAN_RX_EID_DIS;
-    CAN1SetFilter(1, config1, CAN_FILTER_EID(0));
+    config2 = CAN_FILTER_EID(0) & CAN_FILTER_EID(1);
+    CAN1SetFilter(1, config1, config2);
 
     /*Set operation mode : loopback to test -> put it in normal when it works*/
     config1 = CAN_IDLE_STOP &
@@ -63,8 +82,26 @@ int main()
 
     /*loop*/
     while(1){
+        /*Send message for test purpose*/
+        while(!CAN1IsTXReady(1));
+        /*send a message
+         *length 8 bytes, filter 1, Standard ID, data frame,
+         *DMAptr, buffer from which to send
+         */
+        CAN1SendMessage(dataTX, 8, 1, 0, 0, C1TXD, 1);
+
         /*Read CAN bus*/
-        
+        while(!CAN1IsRXReady(0));
+        /*Receive buffer is full*/
+        CAN1ReceiveMessage(data, 8, C1RXD);
+
+        /*Test if the message transmission is successful
+         *If yes turn the led on
+         */
+        if(memcmp(data, dataTX, 8) == 0){
+            LEDon(1);
+        }
+
         /*If we have a request for informations*/
 
         /*If we have a request to change some settings on device*/
